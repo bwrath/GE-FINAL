4edd3609f36c7a034a4aab11f3adcb9d446ab241
FlintWKH <flint.wongkh@gmail.com>
2022-11-09 10:40:39 +0800

    UpdatedUI

---
+++ GE1/Assets/MainMenu/MainMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private void Start()
+    {
+        UnlockMouse();
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    void UnlockMouse()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+    public void HomeScreen()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
