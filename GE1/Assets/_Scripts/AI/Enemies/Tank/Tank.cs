aa300b346eb061b745012b60d6a8a639f53ec69c
GohTheDev <seanmaxgoh@gmail.com>
2022-11-10 09:45:41 +0800

    Minor fixes

--- GE1/Assets/_Scripts/AI/Enemies/Tank/Tank.cs
+++ GE1/Assets/_Scripts/AI/Enemies/Tank/Tank.cs
@@ -40,6 +40,8 @@ public class Tank : Enemy
     {
         base.Update();
 
+        Debug.Log(DetectingWall);
+        Debug.Log(DetectingLedge);
         anim.SetFloat("velocity", navMeshAgent.velocity.magnitude);
     }
 
